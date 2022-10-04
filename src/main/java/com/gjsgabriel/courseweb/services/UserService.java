d166d4ff1ff3467fb9ddc02993f5b17f2e8dd4ab
gjsgabriel <gjsgabriel83@gmail.com>
2022-10-04 14:14:02 -0300

    Service layer, component registration

---
+++ src/main/java/com/gjsgabriel/courseweb/services/UserService.java
@@ -0,0 +1,27 @@
+package com.gjsgabriel.courseweb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gjsgabriel.courseweb.entities.User;
+import com.gjsgabriel.courseweb.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
